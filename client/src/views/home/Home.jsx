4422602b3271ed6174d2d35e05c2728b07113afb
Leon Suarez <leonsua@gmail.com>
2022-10-26 14:18:56 -0400

    added layout to home view

---
+++ client/src/views/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles.css';
+
+export const Home = () => {
+  return(
+    <div className='home-layout'>
+      <div className='header'>header</div>
+      <div className='add-posts'>posting</div>
+      <div className='pictures'>pictures</div>
+      <div className='view-posts'>view</div>
+    </div>
+  )
+}
