c20b1edb3e9c0aa73db6116d70200470f6d423d8
Leon Suarez <leonsua@gmail.com>
2022-11-17 13:31:56 -0500

    working on image diplay section

--- client/src/components/pictures-display/PicturesDisplay.jsx
+++ client/src/components/pictures-display/PicturesDisplay.jsx
@@ -1,22 +1,62 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PostContext } from '../../context/post-context';
 
 import './styles.css';
 
 export const PicturesDisplay = () => {
+  const postContext = useContext(PostContext);
+  const posts = postContext.newPost;
+
   return (
       <div className='pictures'>
         <h4 className='pictures-title'>Fotos</h4>
         <div className='pictures-display'>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
-          <p>picture</p>
+          <div className='left-top-one'>
+            <img src=''/>
+          </div>
+          <div className='left-top-two'>
+            <img src=''/>
+          </div>
+          <div className='left-top-three'>
+            <img src=''/>
+          </div>
+          <div className='center-top'>
+            <img src=''/>
+          </div>
+          <div className='right-top'>
+            <img src=''/>
+          </div>
+          <div className='left-center'>
+            <img src=''/>
+          </div>
+          <div className='center-center-one'>
+            <img src=''/>
+          </div>
+          <div className='center-center-two'>
+            <img src=''/>
+          </div>
+          <div className='center-center-three'>
+            <img src=''/>
+          </div>
+          <div className='right-center'>
+            <img src=''/>
+          </div>
+          <div className='left-bottom'>
+            <img src=''/>
+          </div>
+          <div className='center-bottom'>
+            <img src=''/>
+          </div>
+          <div className='right-bottom-one'>
+            <img src=''/>
+          </div>
+          <div className='right-bottom-two'>
+            <img src=''/>
+          </div>
+          <div className='right-bottom-three'>
+            <img src=''/>
+          </div>
         </div>
       </div>
   )
-}
+};
